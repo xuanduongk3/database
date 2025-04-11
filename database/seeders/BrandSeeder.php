85ed9c12d8a1118ad3c3b8c9726bcd3ae3802b59
xuanduongk3 <duongnguyen20031025@gmail.com>
2025-04-11 14:01:55 +0700

    update banner

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Brand;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class BrandSeeder extends Seeder
@@ -13,44 +12,55 @@ class BrandSeeder extends Seeder
      */
     public function run(): void
     {
-        //
         $data = [
             [
-                'brand_name' => 'Thiên Long'
+                'brand_name' => 'Thiên Long',
+                'description' => 'Thương hiệu văn phòng phẩm hàng đầu Việt Nam, chuyên sản xuất bút viết, dụng cụ học tập và văn phòng.'
             ],
             [
-                'brand_name' => 'Hồng Hà'
+                'brand_name' => 'Hồng Hà',
+                'description' => 'Thương hiệu nổi tiếng với các sản phẩm tập vở, giấy và dụng cụ học sinh chất lượng cao.'
             ],
             [
-                'brand_name' => 'Elephant'
+                'brand_name' => 'Elephant',
+                'description' => 'Thương hiệu văn phòng phẩm nổi tiếng từ Thái Lan, chuyên về dụng cụ bấm kim, kéo, dao rọc giấy.'
             ],
             [
-                'brand_name' => 'Pilot'
+                'brand_name' => 'Pilot',
+                'description' => 'Thương hiệu Nhật Bản chuyên về bút cao cấp như bút gel, bút máy, bút bi và bút lông dầu.'
             ],
             [
-                'brand_name' => 'Deli'
+                'brand_name' => 'Deli',
+                'description' => 'Hãng văn phòng phẩm đến từ Trung Quốc, cung cấp đa dạng sản phẩm từ bút viết, dụng cụ văn phòng đến balo và túi xách.'
             ],
             [
-                'brand_name' => 'Pentel'
+                'brand_name' => 'Pentel',
+                'description' => 'Thương hiệu Nhật Bản chuyên về bút lông, bút chì kim, màu vẽ và các sản phẩm sáng tạo.'
             ],
             [
-                'brand_name' => 'Campus'
+                'brand_name' => 'Campus',
+                'description' => 'Thương hiệu vở viết và sổ ghi chép cao cấp từ Nhật Bản, nổi tiếng với chất lượng giấy tốt.'
             ],
             [
-                'brand_name' => 'Zebra'
+                'brand_name' => 'Zebra',
+                'description' => 'Hãng bút Nhật Bản nổi tiếng với các dòng bút bi, bút lông kim và bút highlight chất lượng cao.'
             ],
             [
-                'brand_name' => 'Plus'
+                'brand_name' => 'Plus',
+                'description' => 'Thương hiệu văn phòng phẩm Nhật Bản chuyên về kéo, dao rọc giấy, bấm kim và các sản phẩm hỗ trợ văn phòng.'
             ],
             [
-                'brand_name' => 'Milan'
+                'brand_name' => 'Milan',
+                'description' => 'Thương hiệu Tây Ban Nha chuyên về tẩy, bút chì, dụng cụ vẽ và sáng tạo cho học sinh và nghệ sĩ.'
             ],
             [
-                'brand_name' => 'Crown'
+                'brand_name' => 'Crown',
+                'description' => 'Thương hiệu văn phòng phẩm từ Hàn Quốc, chuyên về bút bi, bút gel và các dụng cụ học tập khác.'
             ]
         ];
-        foreach ($data as $data) {
-            Brand::create($data);
+
+        foreach ($data as $item) {
+            Brand::create($item);
         }
     }
 }
--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Supplier;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class SupplierSeeder extends Seeder
@@ -13,51 +12,89 @@ class SupplierSeeder extends Seeder
      */
     public function run(): void
     {
-        //
         $data = [
             [
-                'supplier_name' => 'Nhà Xuất Bản Kim Đồng'
+                'supplier_name' => 'Nhà Xuất Bản Kim Đồng',
+                'phone' => '024-39364930',
+                'address' => '55 Quang Trung, Hoàn Kiếm, Hà Nội',
+                'email' => 'contact@kimdong.vn',
             ],
             [
-                'supplier_name' => 'NXB Trẻ'
+                'supplier_name' => 'NXB Trẻ',
+                'phone' => '028-38297806',
+                'address' => '161B Lý Chính Thắng, Phường 7, Quận 3, TP.HCM',
+                'email' => 'nxbtre@nxbtre.com.vn',
             ],
             [
-                'supplier_name' => 'Đinh Tị'
+                'supplier_name' => 'Đinh Tị',
+                'phone' => '024-37955554',
+                'address' => 'Số 26, Ngõ 186 Khương Trung, Thanh Xuân, Hà Nội',
+                'email' => 'info@dinhtibooks.com.vn',
             ],
             [
-                'supplier_name' => 'Nhã Nam'
+                'supplier_name' => 'Nhã Nam',
+                'phone' => '024-37956686',
+                'address' => 'Số 59 Đỗ Quang, Trung Hòa, Cầu Giấy, Hà Nội',
+                'email' => 'info@nhanam.vn',
             ],
             [
-                'supplier_name' => 'Tân Việt'
+                'supplier_name' => 'Tân Việt',
+                'phone' => '024-35145336',
+                'address' => 'Số 2 Duy Tân, Cầu Giấy, Hà Nội',
+                'email' => 'info@tanviet.vn',
             ],
             [
-                'supplier_name' => 'Thái Hà'
+                'supplier_name' => 'Thái Hà',
+                'phone' => '024-37912623',
+                'address' => '53 Phạm Thận Duật, Mai Dịch, Cầu Giấy, Hà Nội',
+                'email' => 'contact@thaihabooks.com',
             ],
             [
-                'supplier_name' => 'Penguin Books'
+                'supplier_name' => 'Penguin Books',
+                'phone' => '+44 20 7139 3000',
+                'address' => '80 Strand, London, UK',
+                'email' => 'info@penguinrandomhouse.com',
             ],
             [
-                'supplier_name' => 'Minh Long'
+                'supplier_name' => 'Minh Long',
+                'phone' => '028-39250156',
+                'address' => 'Số 51 Trần Nhân Tôn, Quận 5, TP.HCM',
+                'email' => 'contact@minhlongbook.vn',
             ],
             [
-                'supplier_name' => 'NXB Tổng Hợp TPHCM'
+                'supplier_name' => 'NXB Tổng Hợp TPHCM',
+                'phone' => '028-39310394',
+                'address' => '62 Nguyễn Thị Minh Khai, Quận 1, TP.HCM',
+                'email' => 'info@nxbhcm.vn',
             ],
             [
-                'supplier_name' => 'Cty Văn Hóa Sách Việt'
+                'supplier_name' => 'Cty Văn Hóa Sách Việt',
+                'phone' => '028-39295550',
+                'address' => '87 Nguyễn Văn Trỗi, Phú Nhuận, TP.HCM',
+                'email' => 'contact@sachviet.vn',
             ],
             [
-                'supplier_name' => 'AZ Việt Nam'
+                'supplier_name' => 'AZ Việt Nam',
+                'phone' => '024-38395533',
+                'address' => 'Số 10, Nguyễn Xiển, Thanh Xuân, Hà Nội',
+                'email' => 'info@azvietnam.vn',
             ],
             [
-                'supplier_name' => 'Văn Phòng Phẩm Stabilo'
+                'supplier_name' => 'Văn Phòng Phẩm Stabilo',
+                'phone' => '028-38775900',
+                'address' => '28 Lý Chính Thắng, Quận 3, TP.HCM',
+                'email' => 'info@stabilo.vn',
             ],
             [
-                'supplier_name' => 'Cty Văn Hóa Việt Thư'
+                'supplier_name' => 'Cty Văn Hóa Việt Thư',
+                'phone' => '024-37286688',
+                'address' => 'Số 23 Nguyễn Chí Thanh, Ba Đình, Hà Nội',
+                'email' => 'contact@vietthu.vn',
             ],
-
         ];
-        foreach($data as $data){
-            Supplier::create($data);
+
+        foreach ($data as $item) {
+            Supplier::create($item);
         }
     }
 }
